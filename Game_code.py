a76e09e31b1f4763dca707de82632b54c6491cd3
vivzoftheGalaxy <88134433+vivzoftheGalaxy@users.noreply.github.com>
2022-02-15 16:22:59 +0530

    Create Game_code.py

---
+++ Game_code.py
@@ -0,0 +1,31 @@
+print("Welcome to ... 'STUPIDITY!' A game for kids 6+! ")
+ask=str(input("Continue?"))
+#This is just a simple input. The 'ask' variable ... thingy doesn't do anything!
+print("Oh, sorry. My ear ... oh wait ... I don't have an ear ... Err ... The computer froze on my end")
+Question1 = input("Continue?")
+if Question1 == ("yes"):
+    print("Okay!")
+    print("System: Continued")
+elif Question1 == ("no"):
+    print("Okay! But you're missing out!")
+Question2 = input("You're in the supermarket. Fart?")
+print(Question2)
+if Question2 == ("yes"):
+        print("What were you thinking!? Everybody is laughing at you!")
+        print("But, at least you lived.")
+elif Question2 == ("no"):
+        print("AAH! You couldn't hold in the fart! You died. Welcome to ... Heaven! You earned the fart ending!")
+        #First ending available!
+
+Question3 = input("You went out of the the store. Ashamed. But, you saw someone giving out free candy! Go there?")
+if Question3 == ("yes"):
+            print("You got free candy! ;D! After that, you found out it was poison that made you depressed. You got depressed.")
+            print("You curled up into a ball and regretted life choices. Big ones. You got depressed! ")
+elif Question3 == ("no"):
+            print("I don't care either way.")
+
+Question4 = input("Go home?")
+if Question4 == ("yes"):
+            print("Well done! You're in a paradise! You watched as much Stupidity as you want! Ending: Good ending! :D")
+elif Question4 == ("no"):
+            print("NNNOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO! Ending: BAD!")
